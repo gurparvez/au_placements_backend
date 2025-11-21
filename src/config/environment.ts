dfc844abd118704e2b09c5659549d7dc610ca2b0
gurparvez <gurparvez04@gmail.com>
2025-11-21 19:45:33 +0530

    feat: update build script in package.json and adjust TypeScript configuration to target ES6

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const CONFIG = {
-  env: process.env.NODE_ENV || 'development',
+  // env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8000,
   mongoUri: process.env.MONGO_URI || '',
 };
